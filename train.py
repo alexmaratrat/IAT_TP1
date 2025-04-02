a12f07aa9d782dfae53003ed14f5d527053f42e8
alexmaratrat <alexandre.maratrat@gmail.com>
2025-04-02 07:31:07 +0000

    Modifying to classification problem

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ path="obstacles_dataset"
 train_dir = f'{path}/train'
 val_dir = f'{path}/test'
 datagen = ImageDataGenerator(rescale=1./255)
-train_generator = datagen.flow_from_directory(train_dir, target_size=(64, 64), batch_size=32,class_mode='binary')
-val_generator = datagen.flow_from_directory(val_dir, target_size=(64, 64), batch_size=32,class_mode='binary')
+train_generator = datagen.flow_from_directory(train_dir, target_size=(64, 64), batch_size=32,class_mode='categorical')
+val_generator = datagen.flow_from_directory(val_dir, target_size=(64, 64), batch_size=32,class_mode='categorical')
 # Définir le modèle CNN
 model = Sequential([
     Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
@@ -17,9 +17,9 @@ model = Sequential([
     MaxPooling2D(pool_size=(2, 2)),
     Flatten(),
     Dense(128, activation='relu'),
-    Dense(1, activation='sigmoid') ])
+    Dense(10, activation='softmax') ])
 # Compilation du modèle
-model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
 # Entraînement du modèle
 model.fit(train_generator, validation_data=val_generator, epochs=10)
 # Évaluation
